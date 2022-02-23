79d4723117cad1b5028eda0c901659f7b3cc8cb2
cristinnamoura <cristinnalp18@gmail.com>
2022-02-22 21:41:30 -0300

    first commit

---
+++ script-cria-banco.sql
@@ -0,0 +1,15 @@
+show databases;
+use test;
+create database bradesco;
+show tables;
+use bradesco;
+
+create table clients(
+	id int primary key auto_increment not null,
+	name varchar(250) not null,
+    conta int not null,
+    agencia int not null,
+    saldo float not null
+);
+
+select * from clients;
